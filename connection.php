702b958308805ad5fb45da99000cb3c8010b2545
Mohit Parekh <54593876+mohitparekh7@users.noreply.github.com>
2021-03-18 10:20:54 +0530

    Add files via upload

--- connection.php
+++ connection.php
@@ -4,5 +4,5 @@ $user_name = "root";
 $password = "";
 $db = "jade";
 $con = mysqli_connect($hostname,$user_name,$password,$db)or die('Unable to connect. Check your connection parameters.');
-mysqli_select_db($con,'food_order') or die(mysqli_error($db));
+mysqli_select_db($con,'jade') or die(mysqli_error($db));
 ?>
